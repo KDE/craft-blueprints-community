be4c395ec81e7f215776dbdb0b6fb6d1a1e237b5
Hannah von Reth <vonreth@kde.org>
2023-01-19 13:09:16 +0100

    Add craft-blueprints-community blueprint for self updates

---
+++ craft/craft-blueprints-community/craft-blueprints-community.py
@@ -0,0 +1,39 @@
+import info
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        for ver in ["master"]:
+            self.svnTargets[ver] = f"https://invent.kde.org/packaging/craft-blueprints-community.git|{ver}|"
+        self.defaultTarget = "master"
+
+    def setDependencies(self):
+        self.buildDependencies["craft/craft-core"] = None
+
+
+from Package.SourceOnlyPackageBase import *
+
+
+class Package(SourceOnlyPackageBase):
+    def __init__(self):
+        SourceOnlyPackageBase.__init__(self)
+        self.subinfo.options.package.disableBinaryCache = True
+        self.subinfo.options.dailyUpdate = True
+
+    def unpack(self):
+        return True
+
+    def install(self):
+        return True
+
+    def qmerge(self):
+        if not SourceOnlyPackageBase.qmerge(self):
+            return False
+        CraftCore.cache.clear()
+        return True
+
+    def createPackage(self):
+        return True
+
+    def checkoutDir(self, index=0):
+        return os.path.join(CraftStandardDirs.blueprintRoot(), self.package.name)
