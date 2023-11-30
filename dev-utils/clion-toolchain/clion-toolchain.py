b5e9ead3851e9722e2a62cc6201bcd273606a89f
Hannah von Reth <vonreth@kde.org>
2023-11-30 15:08:04 +0100

    Add unix support

--- dev-utils/clion-toolchain/clion-toolchain.py
+++ dev-utils/clion-toolchain/clion-toolchain.py
@@ -1,13 +1,12 @@
 # SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
 # SPDX-FileCopyrightText: 2023 Hannah von Reth <vonreth@kde.org>
 
+import shlex
+
 import info
 
 
 class subinfo(info.infoclass):
-    def registerOptions(self):
-        self.parent.package.categoryInfo.platforms = CraftCore.compiler.Platforms.Windows
-
     def setTargets(self):
         self.svnTargets["latests"] = ""
         self.defaultTarget = "latests"
@@ -24,12 +23,23 @@ class Package(VirtualPackageBase):
     def install(self):
         if not self.cleanImage():
             return False
-        batPath = self.imageDir() / "etc/clion-craftenv.bat"
-        if not utils.createDir(batPath.parent):
-            return False
-        with batPath.open("wt", encoding="UTF-8", newline="\r\n") as out:
-            lines = [f"@echo off", "rem This file is auto generated, don't change it", "rem To update it run craft -i clion-toolchain"] + [
-                f"set {k}={v}" for k, v in os.environ.items()
-            ]
-            out.write("\n".join(lines))
-        return True
+        if CraftCore.compiler.isWindows:
+            scriptPath = self.imageDir() / "etc/clion-craftenv.bat"
+            if not utils.createDir(scriptPath.parent):
+                return False
+            with scriptPath.open("wt", encoding="UTF-8", newline="\r\n") as out:
+                lines = [f"@echo off", "rem This file is auto generated, don't change it", "rem To update it run craft -i clion-toolchain"] + [
+                    f"set {k}={v}" for k, v in os.environ.items()
+                ]
+                out.write("\n".join(lines))
+            return True
+        else:
+            scriptPath = self.imageDir() / "etc/clion-craftenv.sh"
+            if not utils.createDir(scriptPath.parent):
+                return False
+            with scriptPath.open("wt", encoding="UTF-8", newline="\r\n") as out:
+                lines = ["# This file is auto generated, don't change it", "# To update it run craft -i clion-toolchain"] + [
+                    f"export {shlex.quote(k)}={shlex.quote(v)}" for k, v in os.environ.items()
+                ]
+                out.write("\n".join(lines))
+            return True
