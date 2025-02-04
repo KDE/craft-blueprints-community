0ac37f928408dca4dcc731f23a278c4413ca8e59
Hannah von Reth <vonreth@kde.org>
2025-02-04 10:36:00 +0100

    Imports

--- dev-utils/clion-toolchain/clion-toolchain.py
+++ dev-utils/clion-toolchain/clion-toolchain.py
@@ -1,9 +1,12 @@
 # SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
 # SPDX-FileCopyrightText: 2023 Hannah von Reth <vonreth@kde.org>
 
+import os
 import shlex
+from pathlib import Path
 
 import info
+import utils
 from Utils.CaseInsensitiveDict import CaseInsensitiveDict
 
 
